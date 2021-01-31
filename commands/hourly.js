52d3f6be2f681bd7cafcf5e75a01aa2dc521a449
jtkim03 <jtk2rw@virginia.edu>
2021-01-31 03:33:44 -0500

    improvement: song looping to one function

--- commands/hourly.js
+++ commands/hourly.js
@@ -4,44 +4,49 @@ function formatAMPM(date) {
     var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
 }
 //TODO: add loops and theme changes
 module.exports = {
-	name: 'hourly',
-	description: 'Plays hourly theme',
-	async execute(message, args) {
-        
+    name: 'hourly',
+    description: 'Plays hourly theme',
+    async execute(message, args) {
+
         const serverQueue = new Map();
         const voiceChannel = message.member.voice.channel;
-        
+
         const connection = await voiceChannel.join();
-        var hour = new Date().getHours()
+        var hour = new Date().getHours();
         time = formatAMPM(new Date)
+        tracker = []
         //plays looped part of song, recursively calling itself
-        function play () {
-            const dispatcher = connection.play(`./hourly/${hour}.mp3`)
-            .on('finish', play);
+        function play(tracker) {
+            if (tracker.length == 0 && (hour === 4 || hour === 7 || hour === 13 || hour === 21 || hour === 17)) {
+                    connection.play(`./hourly/${hour}_intro.mp3`).on('finish', () => {
+                        tracker.push('x');
+                        play(tracker);
+                    })
+                }
+            else {
+                connection.play(`./hourly/${hour}.mp3`)
+                    .on('finish', () => {
+                        play(tracker)
+                    });
+            }
         }
 
-        if(!voiceChannel) {
+        if (!voiceChannel) {
             return message.channel.send('You need to be in a voice channel to execute this command.')
         }
-        
+
         voiceChannel.join().then(connection => {
-        if (hour === 4 || hour === 7 || hour === 13 ||  hour === 21 || hour === 17) {  //songs with intros
-                //plays intro before loop
-                connection.play(`./hourly/${hour}_intro.mp3`).on('finish',play)
-            }
-        else {
-            connection.play(`./hourly/${hour}.mp3`).on('finish',play)
-            
-        }
+
+            play(tracker);
 
         })
-            
-            await message.channel.send(`:microphone: It is now ` + time)
+
+        await message.channel.send(`:microphone: It is now ` + time)
     }
 }
